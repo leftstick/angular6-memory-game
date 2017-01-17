4bba1c628d046a72e97ce94dcd0dfe650bbcc157
Howard.Zuo <leftstick@qq.com>
2017-01-17 16:59:42 +0800

    demo

--- ts/core/helper/object.ts
+++
@@ -1,8 +0,0 @@
-
-export function isEmpty(obj: Object) {
-    return obj === null || obj === undefined;
-}
-
-export function isUndefined(obj: Object) {
-    return obj === undefined;
-}
--- ts/store/model/card.ts
+++
@@ -1,8 +0,0 @@
-
-export interface ICard {
-    flipped: Boolean;
-    name: String;
-}
-
-export const CARDS: String[] = ['8-ball', 'kronos', 'baked-potato', 'dinosaur', 'rocket', 'skinny-unicorn',
-    'that-guy', 'zeppelin'];
