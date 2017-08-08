7dd46cfd4fb24edf9f9732c7b1c0b9aafaa1ce38
Howard.Zuo <leftstick@qq.com>
2017-08-08 17:44:36 +0800

    add prod build

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import './vendors';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
---
+++ src/vendors.ts
@@ -0,0 +1,16 @@
+/**
+ * This file includes third party libraries needed by Memory Game.
+ */
+
+/** @angular stuff **/
+import '@angular/core';
+import '@angular/platform-browser';
+
+
+/** redux stuff **/
+import 'redux';
+import '@angular-redux/store';
+
+/** rx stuff **/
+import 'rxjs/Observable';
+
